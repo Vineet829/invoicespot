1e0bb00fc53249abf4168388925922a62f768561
Vineet829 <vineetsingh5987@gmail.com>
2024-04-24 06:41:46 +0530

    apply final changles

--- invoice-spot/docker/server_deploy.sh
+++ invoice-spot/docker/server_deploy.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-if [ -z "$GOOGLE_IP_ADDRESS"]
-then
-  echo "GOOGLE_IP_ADDRESS not defined"
-  exit 0
-fi
+# if [ -z "$DIGITAL_OCEAN_IP_ADDRESS"]
+# then
+#   echo "DIGITAL_OCEAN_IP_ADDRESS not defined"
+#   exit 0
+# fi
 
 git archive --format tar --output ./project.tar main
 
 echo 'Uploading project.........:-)...Be Patient!'
-rsync ./project.tar root@$GOOGLE_IP_ADDRESS:/tmp/project.tar
+gcloud compute scp ./project.tar root@$YOUR_VM_NAME:/tmp/project.tar --zone $YOUR_VM_ZONE --project $YOUR_PROJECT_ID
+
 echo 'Upload complete'
 
 echo 'Building the image...'
-ssh -o StrictHostKeyChecking=no root@$GOOGLE_IP_ADDRESS << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   mkdir -p /app
   rm -rf /app/* && tar -xf /tmp/project.tar -C /app
   docker-compose -f /app/production.yml build
