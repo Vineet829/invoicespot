cac9d100dd6a1e2fa052153240e2bb91c22e81a4
Vineet829 <vineetsingh5987@gmail.com>
2024-08-05 00:54:30 +0530

    feat: get and update user profile

--- invoice-spot/backend/controllers/user/updateUserProfile.js
+++ invoice-spot/backend/controllers/user/updateUserProfile.js
@@ -5,6 +5,7 @@ import User from "../../models/userModel.js";
 // $-path    PATCH /api/v1/user/profile
 // $-auth    Private
 
+
 const updateUserProfile = asyncHandler(async (req, res) => {
 	const userId = req.user._id;
 
