3f097fbf1bdc13304b1b9f74af99253f3827d9b4
Vineet829 <vineetsingh5987@gmail.com>
2024-08-05 00:53:04 +0530

    feat: add delete my user account

--- invoice-spot/backend/controllers/user/deleteMyAccount.js
+++ invoice-spot/backend/controllers/user/deleteMyAccount.js
@@ -4,6 +4,7 @@ import User from "../../models/userModel.js";
 // $-title   Delete My Account
 // $-path    DELETE /api/v1/user/profile
 // $-auth    Private
+
 const deleteMyAccount = asyncHandler(async (req, res) => {
 	const userId = req.user._id;
 
