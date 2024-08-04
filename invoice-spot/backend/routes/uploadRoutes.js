82f73d20a962ee0757c446383b31b24354a1a24f
Vineet829 <vineetsingh5987@gmail.com>
2024-08-05 00:56:17 +0530

    feat: upload image to cloudinary using multer

--- invoice-spot/backend/routes/uploadRoutes.js
+++ invoice-spot/backend/routes/uploadRoutes.js
@@ -4,6 +4,7 @@ import upload from "../helpers/multer.js";
 
 const router = express.Router();
 
+
 router.route("/").patch(upload.single("logo"), async (req, res) => {
 	const localFilePath = req.file.path;
 	const result = await cloudinaryUploader(localFilePath);
