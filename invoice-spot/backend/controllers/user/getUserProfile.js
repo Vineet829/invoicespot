9385a67718115dd98865867fd7cbb774fbe06b46
Vineet829 <vineetsingh5987@gmail.com>
2024-08-05 00:54:30 +0530

    feat: get and update user profile

--- invoice-spot/backend/controllers/user/getUserProfile.js
+++ invoice-spot/backend/controllers/user/getUserProfile.js
@@ -5,6 +5,7 @@ import User from "../../models/userModel.js";
 // $-path    GET /api/v1/user/profile
 // $-auth    Private
 
+
 const getUserProfile = asyncHandler(async (req, res) => {
 	const userId = req.user._id;
 
