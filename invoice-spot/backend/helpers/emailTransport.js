df3c130c21a69ebfa1fab496e95c18b8bf3eacb9
Vineet829 <vineetsingh5987@gmail.com>
2025-01-06 22:49:44 +0530

    apply final changes

--- invoice-spot/backend/helpers/emailTransport.js
+++ invoice-spot/backend/helpers/emailTransport.js
@@ -2,34 +2,34 @@ import "dotenv/config";
 import nodemailer from "nodemailer";
 import { google } from "googleapis";
 let transporter
-const oAuth2Client = new google.auth.OAuth2(
-	process.env.GOOGLE_CLIENT_ID,
-	process.env.GOOGLE_CLIENT_SECRET,
-	process.env.GOOGLE_CALLBACK_URL
-  );
+// const oAuth2Client = new google.auth.OAuth2(
+// 	process.env.GOOGLE_CLIENT_ID,
+// 	process.env.GOOGLE_CLIENT_SECRET,
+// 	process.env.GOOGLE_CALLBACK_URL
+//   );
 
 
-  oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+//   oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-if (process.env.NODE_ENV === "development") {
-    transporter = nodemailer.createTransport({
-        host: "mailhog",
-        port: 1025,
-    });
-} else if (process.env.NODE_ENV === "production") {
-	const accessToken = await oAuth2Client.getAccessToken();
-    transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: "vineetsingh5987@gmail.com",
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: accessToken,
-      },
-    });
-}
+// if (process.env.NODE_ENV === "development") {
+//     transporter = nodemailer.createTransport({
+//         host: "mailhog",
+//         port: 1025,
+//     });
+// } else if (process.env.NODE_ENV === "production") {
+// 	const accessToken = await oAuth2Client.getAccessToken();
+//     transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         type: "OAuth2",
+//         user: "vineetsingh5987@gmail.com",
+//         clientId: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         refreshToken: process.env.REFRESH_TOKEN,
+//         accessToken: accessToken,
+//       },
+//     });
+// }
 
 
 export default transporter;
