b1ab3cf87af629876e5d94d5c94b7363d894ee1d
Vineet829 <vineetsingh5987@gmail.com>
2026-08-26 04:50:03 +0530

    feat: scaffold spring boot project

---
+++ src/main/java/com/invoicespot/InvoiceSpotApplication.java
@@ -0,0 +1,12 @@
+package com.invoicespot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceSpotApplication.class, args);
+    }
+}
